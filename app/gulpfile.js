e6cb2116f82054e21f2de7a62670bd420828ebe1
Aliaksei Syrel <alex@syrel.ch>
2016-09-02 21:51:33 +0200

    Integrated requirejs

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -2,8 +2,8 @@ var gulp = require('gulp');
 var $    = require('gulp-load-plugins')();
 
 var sassPaths = [
-  'bower_components/foundation-sites/scss',
-  'bower_components/motion-ui/src'
+  'assets/foundation-sites/scss',
+  'assets/motion-ui/src'
 ];
 
 gulp.task('sass', function() {
--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,21 @@
-$(document).foundation();
+requirejs.config({
+    paths: {
+        "jquery": '../assets/jquery/dist/jquery.min',
+        "foundation": '../assets/foundation-sites/dist/foundation.min',
+        "what-input": '../assets/what-input/what-input.min',
+        "motion-ui": '../assets/motion-ui/dist/motion-ui.min'
+    },
+    shim: {
+        "foundation": ['jquery']
+    }
+});
+
+// Add any foundation modules you require to the end of this line.
+require(['jquery', 'foundation', 'what-input'], function($) {
+    // Foundation JavaScript
+    // Documentation can be found at: http://foundation.zurb.com/docs
+    $(document).load(function() {
+        $(this).foundation();
+        alert('haba');
+    });
+});
